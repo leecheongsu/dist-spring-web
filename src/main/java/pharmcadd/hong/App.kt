6655d1ffca19d151ed11f7bc310efb1d2092d0cd
이용홍 <hong@pharmcadd.com>
2021-11-19 16:30:13 +0900

    init

---
+++ src/main/java/pharmcadd/hong/App.kt
@@ -0,0 +1,27 @@
+package pharmcadd.hong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    fun test(): Map<String, Int> {
+        return mapOf(
+            "foo" to 1,
+            "bar" to 2
+        )
+    }
+}
+
+@SpringBootApplication
+class App {
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+}
